d41f055bed02c93491fde2aa73232eb74aa6d060
jezuzdlc <jesusdlcode@gmail.com>
2025-07-20 14:44:07 -0600

    button

--- src/Components/Layout/Button/Button.jsx
+++ src/Components/Layout/Button/Button.jsx
@@ -1,7 +1,13 @@
 import styles from "./Button.module.css"
 
-export const Button = ({children,text,handleClick})=>{
+export const Button = ({children,text,handleClick,width='100'})=>{
+    
+    const customButton = {
+    width:`${width}px`,
+    maxWidth:`130px`
+    }
+
     return(
-        <button className={styles.button} onClick={handleClick} type="button"> {text} {children}</button>
+        <button className={styles.button} style={customButton} onClick={handleClick} type="button"> {text} {children}</button>
     )
 }
